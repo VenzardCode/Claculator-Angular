0c890c1362ceb9db4ea7535612ffeee8667a06eb
Venzard <assasin0084@gmail.com>
2021-11-02 00:53:56 +0200

    Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,36 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import {RouterModule, Routes} from "@angular/router";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
 
+
+const routes: Routes=[
+  {
+    path:'calculator',
+    loadChildren:()=>import('./calculator/calculator.module').then(m=>m.CalculatorModule)
+  },
+  {
+
+    path:'',
+    redirectTo:'calculator',
+    pathMatch:'full'
+  }
+
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    MatButtonModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
